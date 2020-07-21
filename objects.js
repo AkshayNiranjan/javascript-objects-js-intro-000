8936262c3c013291676b98ad8ebb70a116c21ef4
Akshay Niranjan <anstmfriend@gmail.com>
2020-07-21 10:05:26 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
 var playlist= {"Billy Mayes":"Special TV Offer"}
 function updatePlaylist(p,a,s){
   p=Object.assign({},p,{[a]:s})
+  console.log(p)
   return p
 }
 function removeFromPlaylist(p,a){
